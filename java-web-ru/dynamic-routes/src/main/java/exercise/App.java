a99aead2cae2e22837882556443e7f8ebcb94ba0
@hexlet/cli <support@hexlet.io>
2025-03-09 19:37:44 +0300

    submit java-web-ru/dynamic-routes

---
+++ java-web-ru/dynamic-routes/src/main/java/exercise/App.java
@@ -0,0 +1,60 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import java.util.Map;
+
+
+public final class App {
+
+    private static final List<Map<String, String>> COMPANIES = Data.getCompanies();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+        });
+
+        app.get("/companies/{id}", ctx -> {
+            String id = ctx.pathParam("id");
+            for(Map<String, String> map : COMPANIES) {
+                if (map.get("id").equals(id)) {
+                    ctx.json(map);
+                    break;
+                }
+            }
+            ctx.status(400).result("Company not found");
+        });
+
+        app.get("/companies", ctx -> {
+            ctx.json(COMPANIES);
+        });
+
+        app.get("/", ctx -> {
+            ctx.result("open something like (you can change id): /companies/5");
+        });
+
+        return app;
+
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
+//
+//В приложении реализуйте обработчик для динамического маршрута /companies/{id}.
+//
+//При GET-запросе на адрес /companies/{id} приложение должно отдавать представление компании в формате json.
+//
+//Данные о компаниях находятся в константе COMPANIES в виде списка List. Каждая компания — это словарь Map<String, String> с ключом id:
+//
+//// Гипотетический пример
+//Map<String, String> company = Map.of(
+//        "id", "3",
+//        "name", "Google"
+//        "telephone", "12345678"
+//)
+//
+//Если компания с таким идентификатором не существует, приложение должно возвращать страницу с HTTP-кодом 404 и текстом Company not found.
